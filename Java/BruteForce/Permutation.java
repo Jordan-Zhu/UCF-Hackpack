9db7d5310063042ddd7c8a02d3ee6b1840481706
Chris <Chris2Schill@gmail.com>
2016-03-20 22:01:53 -0400

    Added Permutations of list elements in java.

---
+++ Java/BruteForce/Permutation.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Permutation{
+
+    // Processes all permutations of list arr in the lower if block.
+    // Call with k = 0
+    public void permute(List<Integer> arr, int k){
+        for(int i = k; i < arr.size(); i++){
+            Collections.swap(arr, i, k);
+            permute(arr, k+1);
+            Collections.swap(arr, k, i);
+
+        }
+        if (k == arr.size() -1){
+            System.out.println(Arrays.toString(arr.toArray()));
+
+        }
+
+    }
+
+    public static void main(String[] args){
+        new Permutation().permute(Arrays.asList(3,4,6,2,1), 0);
+    }
+
+}
