4bc868267378fece51d8a09a369b70ea38ee46fd
Chris <Chris2Schill@gmail.com>
2016-03-21 12:58:17 -0400

    Fixed spacing

--- Java/BruteForce/Permutation.java
+++ Java/BruteForce/Permutation.java
@@ -2,20 +2,18 @@ import java.util.*;
 
 public class Permutation{
 
-    // Processes all permutations of list arr in the lower if block.
+    // Processes all permutations of List arr in the lower if block.
     // Call with k = 0
     public void permute(List<Integer> arr, int k){
         for(int i = k; i < arr.size(); i++){
             Collections.swap(arr, i, k);
             permute(arr, k+1);
             Collections.swap(arr, k, i);
-
         }
+
         if (k == arr.size() -1){
             System.out.println(Arrays.toString(arr.toArray()));
-
         }
-
     }
 
     public static void main(String[] args){
