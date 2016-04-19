eeb6d6811eb5c5854b2eafe72cedb6f99c1741c2
Chris <Chris2Schill@gmail.com>
2016-04-19 12:20:03 -0400

    Added GCD.java

---
+++ Java/BruteForce/GCD.java
@@ -0,0 +1,15 @@
+public class GCD{
+
+    public static int gcd(int a, int b) {
+        while(a!=0 && b!=0){ // until either one of them is 0 
+            int c = b;
+            b = a%b;
+            a = c;
+
+        }
+        return a+b; // either one is 0, so return the non-zero value
+    }
+    public static void main(String[] args){
+        System.out.println(gcd(12,72));
+    }
+}
