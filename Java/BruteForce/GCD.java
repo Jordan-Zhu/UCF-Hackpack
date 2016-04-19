8880fba0a2297aad38c5cb9373cc5f4e8755884e
Chris <Chris2Schill@gmail.com>
2016-04-19 12:20:54 -0400

    Cleaned up GCD.java

--- Java/BruteForce/GCD.java
+++ Java/BruteForce/GCD.java
@@ -1,11 +1,10 @@
 public class GCD{
 
     public static int gcd(int a, int b) {
-        while(a!=0 && b!=0){ // until either one of them is 0 
+        while(a != 0 && b != 0){ // until either one of them is 0 
             int c = b;
             b = a%b;
             a = c;
-
         }
         return a+b; // either one is 0, so return the non-zero value
     }
