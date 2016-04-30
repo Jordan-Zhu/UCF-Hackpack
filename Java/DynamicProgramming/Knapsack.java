535cc0df3e324b91784ccea2aac26697d669da21
Jordan Zhu <qwertybot1@gmail.com>
2016-04-30 15:49:20 -0400

    Added files via upload

--- Java/DynamicProgramming/Knapsack.java
+++ Java/DynamicProgramming/Knapsack.java
@@ -9,12 +9,15 @@ public class Knapsack {
         {
             for(int w = 0; w <= capacity; w++)
             {
-                if(i == 0 || w == 0)
+                if(i == 0 || w == 0) {
                     K[i][w] = 0;
-                else if(weight[i - 1] <= w)
+                }
+                else if(weight[i - 1] <= w) {
                     K[i][w] = Math.max(val[i - 1] + K[i - 1][w - weight[i - 1]], K[i - 1][w]);
-                else
+                }
+                else {
                     K[i][w] = K[i - 1][w];
+                }
             }
         }
         return K[numItems][capacity];
